25dbfd3ef06cde6b63e177072c8edede74d6907d
MATTHEW-KUHR-DE\Matthew <kuhrm9095@gmail.com>
2024-09-18 09:20:22 -0400

    fix exclusive tags with transitions

--- com.vrcfury.vrcfury/Editor/VF/Feature/ToggleBuilder.cs
+++ com.vrcfury.vrcfury/Editor/VF/Feature/ToggleBuilder.cs
@@ -252,7 +252,7 @@ namespace VF.Feature {
                 if (clip.GetAllBindings().Length == 0) {
                     clip = inClip.GetLastFrame();
                 }
-                var outClip = model.simpleOutTransition ? inClip.Clone() : actionClipService.LoadState(onName + " Out", outAction);
+                var outClip = model.simpleOutTransition ? (originalInAction == null ? inClip.Clone() : actionClipService.LoadState(onName + " Out", originalInAction)) : actionClipService.LoadState(onName + " Out", outAction);
                 var outSpeed = model.simpleOutTransition ? -1 : 1;
                 
                 // Copy "object enabled" and "material" states to in and out clips if they don't already have them
